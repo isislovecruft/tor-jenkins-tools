0a9dbc14e7b38af50f270032fd383ede2e5c5a8c
Peter Palfrader <peter@palfrader.org>
2017-12-01 11:31:28 +0100

    try to fix paths

--- slaves/linux/common.sh
+++ slaves/linux/common.sh
@@ -52,16 +52,17 @@ relay_to_remote() {
 			build-arm-0[0-3].torproject.org)
 				if [ -n "$sync_to_remote" ]; then
 					echo "[$hostname] Syncing $sync_to_remote to $NODE_NAME"
-					fp=$(readlink -f ./$sync_to_remote)
+					fp=$(realpath --relative-to=/home/jenkins ./$sync_to_remote)
 					ssh "$NODE_NAME" "mkdir -p $(dirname $fp)"
 					rsync -Pravz --delete "$fp" "$NODE_NAME:$fp"
 				fi
 				echo "[$hostname] Forwarding build request to $NODE_NAME."
 				set -x
-				ssh -o BatchMode=yes -tt "$NODE_NAME" "(cd jenkins-tools && git pull) && NODE_NAME='$NODE_NAME' SUITE='$SUITE' ARCHITECTURE='$ARCHITECTURE' JOB_NAME='$JOB_NAME' jenkins-tools/$what"
+				fp=$(realpath --relative-to=/home/jenkins .)
+				ssh -o BatchMode=yes -tt "$NODE_NAME" "(cd jenkins-tools && git pull) && cd '$(fp)' && NODE_NAME='$NODE_NAME' SUITE='$SUITE' ARCHITECTURE='$ARCHITECTURE' JOB_NAME='$JOB_NAME' ~/jenkins-tools/$what"
 				if [ -n "$sync_from_remote" ]; then
 					echo "[$hostname] Syncing $sync_from_remote from $NODE_NAME"
-					fp=$(readlink -f ./$sync_from_remote)
+					fp=$(realpath --relative-to=/home/jenkins -f ./$sync_from_remote)
 					rsync -Pravz --delete "$NODE_NAME:$fp" "$fp"
 				fi
 				;;
